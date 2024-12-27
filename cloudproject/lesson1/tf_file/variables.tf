824045c1a59f4016f32ef781c4061c016381e898
Anton Stogov <97850376+AntonStogov@users.noreply.github.com>
2024-12-27 19:44:07 +0500

    Add files via upload

---
+++ cloudproject/lesson1/tf_file/variables.tf
@@ -0,0 +1,48 @@
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "zone_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "public_cidr" {
+  type        = list(string)
+  default     = ["192.168.10.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "private_cidr" {
+  type        = list(string)
+  default     = ["192.168.20.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "new_VPC"
+  description = "VPC network"
+}
+
+variable "public_subnet" {
+  type        = string
+  default     = "public"
+  description = "subnet name"
+}
+
+variable "private_subnet" {
+  type        = string
+  default     = "private"
+  description = "subnet name"
+}
